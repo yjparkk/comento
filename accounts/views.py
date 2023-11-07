f211ff43e6e5675b98bd9edc74c64d2162553713
yjparkk <qkrdbwll@naver.com>
2023-11-07 17:06:03 +0900

    3주차 완료

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from accounts.forms import SignupForm
 
-# Create your views here.
+def signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = SignupForm()
+
+    return render(request, 'accounts/signup.html', {'form': form})
